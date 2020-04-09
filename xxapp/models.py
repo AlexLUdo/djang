b619e4d2dd8933eb06f035ce0535253f93be428f
Aleksei <55854439+AlexLUdo@users.noreply.github.com>
2020-04-09 21:49:09 +0300

    Revert " ДЗ 24"

--- xxapp/models.py
+++ xxapp/models.py
@@ -19,34 +19,26 @@ class Vacancy(TimeStamp, models.Model):
     vac = models.CharField(max_length=30, verbose_name='Вакансия', null = True, blank=True)
     reg = models.CharField( max_length=30, verbose_name='Регион', null = True, blank=True)
     num = models.IntegerField(verbose_name='Количество', null = True, blank=True)
-    user = models.ForeignKey(XxUser, on_delete = models.CASCADE, null = True, blank=True)
+    user = models.ForeignKey(XxUser, on_delete = models.CASCADE)
     #null = True, blank=True - записать пустыми данными создаваемые поля
 
-    def some_met(self):
-        return 'Who Are You?'
-
     def __str__(self):
-         return f'{self.vac} {self.reg}  {self.num}'
-
-    # @classmethod
-    # def create_user(cls, username, email, password):
-    #     pass
-
+        return f'{self.vac} {self.reg}  {self.num}'
 
 
 class Skill(TimeStamp, models.Model):
 
     skl = models.CharField(max_length=300, verbose_name='Требуемые знания', null = True, blank=True)
     reg = models.CharField(max_length=30, verbose_name='Регион', null = True, blank=True)
-    user = models.ForeignKey(XxUser, on_delete = models.CASCADE, null = True, blank=True)
+
     def __str__(self):
         return f'{self.skl} {self.reg}'
 
 
 class Article(TimeStamp, models.Model):
 
-    art = models.TextField(blank=True,null = True)
-    ul = models.URLField(max_length=256,null = True, blank=True)
+    art = models.TextField(blank=True)
+    ul = models.URLField(max_length=256)
 
     def __str__(self):
         return f'{self.art} {self.ul}'
